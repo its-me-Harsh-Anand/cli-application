2072641ff85292c9c8681121a9635c5b42f9c06f
its-me-Harsh-Anand <harshvivek.787@gmail.com>
2022-01-15 14:13:23 +0530

    details updated

--- bin/index.js
+++ bin/index.js
@@ -4,4 +4,6 @@ console.log(`
 Name: Harsh Anand
 Website: https://learnwithharsh.me
 GitHub: https://github.com/its-me-Harsh-Anand
+LinkedIn: https://www.linkedin.com/in/harsh-anand-3b369a210
+Stack Overflow: https://stackoverflow.com/users/17401920/harsh-anand
 `);
